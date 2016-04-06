70c4a16196defbe1a82e0f9f750a4811b93c805b
javflores <jflores@findmypast.com>
2016-04-06 07:04:00 +0100

    Creating wrapper for the application process to be used in Acceptance tests. This way we can hide the process internals from the steps specification.

---
+++ AcceptanceTests/Infrastructure/TheNewTwitterApplication.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AcceptanceTests.Infrastructure
+{
+    public class TheNewTwitterApplication
+    {
+        readonly Process _application;
+
+        public TheNewTwitterApplication()
+        {
+            _application = new Process();
+            string TheNewTwitterApplicationPath = $"{AppDomain.CurrentDomain.BaseDirectory}/CommandLine.exe";
+
+            var startInfo = new ProcessStartInfo(TheNewTwitterApplicationPath)
+            {
+                UseShellExecute = false,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true
+            };
+
+            _application.StartInfo = startInfo;
+        }
+
+        public void Start()
+        {
+            _application.Start();
+        }
+
+        public void PublishMessage(string message)
+        {
+            _application.StandardInput.WriteLine(message);
+        }
+
+        public string ReadConsole()
+        {
+            return _application.StandardOutput.ReadLine();
+        }
+
+        public void Stop()
+        {
+            _application.Kill();
+        }
+    }
+}
--- AcceptanceTests/Steps/PostingSteps.cs
+++ AcceptanceTests/Steps/PostingSteps.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using AcceptanceTests.Infrastructure;
 using Should;
 using TechTalk.SpecFlow;
 
@@ -8,39 +9,29 @@ namespace AcceptanceTests.Steps
     [Binding]
     public class PostingSteps
     {
-        readonly string TheNewTwitterApplicationPath = $"{AppDomain.CurrentDomain.BaseDirectory}/CommandLine.exe";
-        Process _theNewTwitter;
+        TheNewTwitterApplication _theNewTwitter;
 
         [Given(@"I have started The New Twitter")]
         public void GivenIHaveStartedTheNewTwitter()
         {
-            _theNewTwitter = new Process();
-            var startInfo = new ProcessStartInfo(TheNewTwitterApplicationPath)
-            {
-                UseShellExecute = false,
-                RedirectStandardInput = true,
-                RedirectStandardOutput = true
-            };
-
-            _theNewTwitter.StartInfo = startInfo;
+            _theNewTwitter = new TheNewTwitterApplication();
             _theNewTwitter.Start();
         }
 
         [When(@"I publish a message to a personal timeline")]
         public void WhenIPublishAMessageToAPersonalTimeline()
         {
-            _theNewTwitter.StandardInput.WriteLine("Alice -> I love the weather today");
+            _theNewTwitter.PublishMessage("Alice -> I love the weather today");
         }
         
         [Then(@"message appears in users timeline")]
         public void ThenMessageAppearsInUsersTimeline()
         {
-            _theNewTwitter.StandardInput.WriteLine("Alice");
-            var timeline = _theNewTwitter.StandardOutput.ReadLine();
-
+            _theNewTwitter.PublishMessage("Alice");
+            var timeline = _theNewTwitter.ReadConsole();
             timeline.ShouldContain("I love the weather today");
 
-            _theNewTwitter.Kill();
+            _theNewTwitter.Stop();
         }
     }
 }
